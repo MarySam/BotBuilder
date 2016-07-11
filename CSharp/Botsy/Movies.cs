1477f95a7e35c79cea63323ed25420aed80566f7
Mary <marysam01@hotmail.com>
2016-07-11 03:23:17 -0700

    Published to Azure.  Added GetDirector method. Edited Web config. Added comments to classes.

--- CSharp/Botsy/Movies.cs
+++ CSharp/Botsy/Movies.cs
@@ -11,18 +11,24 @@ namespace Botsy
 {
     public class Movies
     {
+        //A method called "GetMovieInfo" that will get info on a movie based on the name of a movie.
         public static async Task<MovieInfo> GetMovieInfoAsync(string movieName)
         {
             if (string.IsNullOrWhiteSpace(movieName))
                 return null;
 
+            //The website below has movie data which are all in json format.
+            //In order to search by title, we added "t=" to the end of the url so that when movieName gets passed in, 
+            //..it will know to search by title in the movie database.
             string url = $"http://www.omdbapi.com/?t=" + movieName;
+
             string json;
+            //Downloads all of the movie data and puts it in our variable called json.
             using (WebClient client = new WebClient())
             {
                 json = await client.DownloadStringTaskAsync(url).ConfigureAwait(false);
             }
-
+            //Converts the json file to a .NET object called "MovieInfo".
             return JsonConvert.DeserializeObject<MovieInfo>(json);
         }
     }
