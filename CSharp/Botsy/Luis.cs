3489c683be6dbe08ff242a54492591b760b656f3
Mary <marysam01@hotmail.com>
2016-07-11 00:07:14 -0700

    Initial commit with GetMovieYear and GetMovieDirector

---
+++ CSharp/Botsy/Luis.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Botsy
+{
+    public class LuisMovieClient
+    {
+        public static async Task<LuisInfo> ParseUserInput(string strInput)
+        {
+            string strRet = string.Empty;
+            string strEscaped = Uri.EscapeDataString(strInput);
+
+            using (var client = new HttpClient())
+            {
+                string uri = "https://api.projectoxford.ai/luis/v1/application?id=27eb0729-4391-4880-a04f-dae911162fe1&subscription-key=a0794768387a459da34bab6f49878c1e&q=" + strEscaped;
+                HttpResponseMessage msg = await client.GetAsync(uri);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await msg.Content.ReadAsStringAsync();
+                    LuisInfo _Data = JsonConvert.DeserializeObject<LuisInfo>(jsonResponse);
+                    return _Data;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class LuisInfo
+    {
+        public string query { get; set; }
+        public Intent[] intents { get; set; }
+        public Entity[] entities { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public float score { get; set; }
+    }
+}
---
+++ CSharp/Botsy/Movies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Botsy
+{
+    public class Movies
+    {
+        public static async Task<MovieInfo> GetMovieInfoAsync(string movieName)
+        {
+            if (string.IsNullOrWhiteSpace(movieName))
+                return null;
+
+            string url = $"http://www.omdbapi.com/?t=" + movieName;
+            string json;
+            using (WebClient client = new WebClient())
+            {
+                json = await client.DownloadStringTaskAsync(url).ConfigureAwait(false);
+            }
+
+            return JsonConvert.DeserializeObject<MovieInfo>(json);
+        }
+    }
+}
